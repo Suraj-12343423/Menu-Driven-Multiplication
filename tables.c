381782738a1814ca91248d34fbea423c91627530
SaiSuraj127 <115217856+Suraj-12343423@users.noreply.github.com>
2026-05-02 11:21:00 +0530

    Add files via upload

---
+++ tables.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int choice, num, limit, i;
+
+    while (1) {
+        printf("\n===== MULTIPLICATION TABLE MENU =====\n");
+        printf("1. Generate Table\n");
+        printf("2. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+
+            printf("\nEnter a number: ");
+            scanf("%d", &num);
+
+            printf("Enter limit: ");
+            scanf("%d", &limit);
+
+            if (limit <= 0) {
+                printf("Please enter a positive limit.\n");
+                continue;
+            }
+
+            printf("\nMultiplication Table of %d:\n\n", num);
+
+            for (i = 1; i <= limit; i++) {
+                printf("%d x %d = %d\n", num, i, num * i);
+            }
+
+        } 
+        else if (choice == 2) {
+            printf("Exiting program... Goodbye!\n");
+            break;
+        } 
+        else {
+            printf("Invalid choice. Try again.\n");
+        }
+    }
+
+    return 0;
+}
